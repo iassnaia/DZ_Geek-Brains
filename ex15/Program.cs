fa7fd5b2a1c9c0d660ab16cb122a08d2a401eabc
iassnaia <iassnaia@mail.ru>
2022-08-25 12:43:23 +0300

    измененя  ex13

---
+++ ex15/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число обозначающее день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+bool findWeekEnd(int weekDay)
+{
+    bool numberDay;
+    if ((weekDay == 6) ^ (weekDay == 7))
+    {
+        numberDay = true;
+    }
+    else
+    {
+        numberDay = false;
+    }
+    return numberDay;
+}
+
+if (findWeekEnd(day) == true)
+{
+    Console.Write("Да, это выходной день");
+}
+else
+{
+    Console.Write("Нет, это рабочий день");
+}
