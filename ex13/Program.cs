153bce83a26edfc80ad82c390c6ab7f7c056770f
iassnaia <iassnaia@mail.ru>
2022-08-25 09:26:12 +0300

    ex13

--- ex13/Program.cs
+++ ex13/Program.cs
@@ -1,17 +1,11 @@
 ﻿Console.WriteLine("Введите число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
-int numberThree(int number)
+int numberB = numberA / 100 % 10;
 {
-    int numberB = number / 10 % 100;
-    return numberB;
-    {
-        if (number < 100)
-            Console.WriteLine("Третьей цифры в заданном нет");
-    }
-    {
-        else
-            int result = SecondNumber(numberA);
-        Console.WriteLine($"Третья цифра числа = {result}");
-    }
+    if (numberA < 100)
+        Console.WriteLine("Третьей цифры в заданном нет");
 
+    else
+
+        Console.WriteLine($"Третья цифра числа = {numberB}");
 }
