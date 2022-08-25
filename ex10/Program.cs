6b82ecaf9f011566eea0246c784403fd53b6dbbe
iassnaia <iassnaia@mail.ru>
2022-08-25 08:46:40 +0300

    ex10

---
+++ ex10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int SecondNumber(int number)
+{
+     int numberB = number / 10 % 10; 
+     return numberB;
+}
+int result = SecondNumber(numberA);
+Console.WriteLine($"Вторая цифра числа = {result}");
