ead2e0e5f9c51155bda4726002b01057db943f1d
iassnaia <iassnaia@mail.ru>
2022-08-25 13:01:45 +0300

    изменения ex10

--- ex10/Program.cs
+++ ex10/Program.cs
@@ -2,8 +2,8 @@
 int numberA = Convert.ToInt32(Console.ReadLine());
 int SecondNumber(int number)
 {
-     int numberB = number / 10 % 10; 
-     return numberB;
+    int numberB = number / 10 % 10;
+    return numberB;
 }
 int result = SecondNumber(numberA);
 Console.WriteLine($"Вторая цифра числа = {result}");
