ad637175c2591ea90df133a21ceeb17c655269a1
iassnaia <iassnaia@mail.ru>
2022-08-27 06:01:48 +0300

    ex19

---
+++ ex19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+
+void PalindromeRow(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  Console.WriteLine($"Число {number} - палиндром.");
+  
+  else 
+  Console.WriteLine($"Число {number} - не палиндром.");
+}
+{
+if (number!.Length == 5)
+   PalindromeRow(number);
+else Console.WriteLine($"Число введено не верно");
+}
